cf7a80e857fb7c44bb294012f35287caa422b740
徐海波 <1484389028@qq.com>
2021-06-21 18:30:05 +0800

    621

--- config.js
+++ config.js
@@ -1,13 +1,34 @@
 const databaseOptions = {
   dialect: 'mysql',
-  host: '119.29.189.246',
+  host: 'localhost',
   database: 'mychat',
   username: 'root',
-  password: 'lkk200033',
+  password: 'root123',
+  // retry: {
+  //   match: [
+  //       /ETIMEDOUT/,
+  //       /EHOSTUNREACH/,
+  //       /ECONNRESET/,
+  //       /ECONNREFUSED/,
+  //       /ETIMEDOUT/,
+  //       /ESOCKETTIMEDOUT/,
+  //       /EHOSTUNREACH/,
+  //       /EPIPE/,
+  //       /EAI_AGAIN/,
+  //       /SequelizeConnectionError/,
+  //       /SequelizeConnectionRefusedError/,
+  //       /SequelizeHostNotFoundError/,
+  //       /SequelizeHostNotReachableError/,
+  //       /SequelizeInvalidConnectionError/,
+  //       /SequelizeConnectionTimedOutError/
+  //   ],
+  //   max: 5
+  // },
 };
 // 用于jwt验证
 const SECRET = 'lkk200033';
 const jwtTimeout = 1800;
+
 exports.databaseOptions = databaseOptions;
 exports.SECRET = SECRET;
 exports.JWTTIMEOUT = jwtTimeout;
